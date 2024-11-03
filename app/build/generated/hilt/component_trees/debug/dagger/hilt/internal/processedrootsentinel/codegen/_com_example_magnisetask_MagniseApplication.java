b257c7cf427af3d2afef06970d33c71064f4b10c
Maksym Kalinichenko <kalinichenkomaksym.s@gmail.com>
2024-11-03 02:04:28 +0200

    Delete temp files

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_magnisetask_MagniseApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.example.magnisetask.MagniseApplication"
-)
-public final class _com_example_magnisetask_MagniseApplication {
-}
